ed495974c8ee8ed6bdf7fe605ffb5291e757abf3
Camden Withrow <cmwithrow@anderson.edu>
2022-06-30 22:12:20 -0400

    alter to specify feelings

--- frontend/app.js
+++ frontend/app.js
@@ -6,12 +6,11 @@ const checkin = document.querySelector("#checkin-time");
 fetch("https://checkoncam.herokuapp.com/")
   .then((response) => response.json())
   .then((data) => {
-    let lastTime = new Date(data.lastUpdated);
-    let now = new Date();
+    const feeling = data.feeling;
+    const emoji = data.emoji;
+    const gif = data.gif;
 
-    let milliToHour = 1000 * 60 * 60;
-
-    let hourDelta = (now - lastTime) / milliToHour;
+    const lastTime = new Date(data.lastUpdated);
 
     let lastCheckIn = lastTime.toLocaleDateString("en-us", {
       year: "numeric",
@@ -23,19 +22,6 @@ fetch("https://checkoncam.herokuapp.com/")
 
     checkin.innerHTML = lastCheckIn;
 
-    let aliveStuff
-
-    if(hourDelta <= 24) {
-        aliveStuff = {text: "Alive :)", color: "#16a34a"}
-    } else if(24 < hourDelta < 36) {
-        aliveStuff = {text: "Alive", color: "#84cc16"}
-    } else if(36 < hourDelta < 48) {
-        aliveStuff = {text: "Alive??", color: "#eab308"}
-    } else {
-        aliveStuff = {text: "Maybe Check In?", color: "#ea580c"}
-    }
-
     alive.innerHTML = aliveStuff.text;
-    alive.style.color = aliveStuff.color
-
+    alive.style.color = aliveStuff.color;
   });
